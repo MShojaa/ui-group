232e80dbf70ef98f7380f6385b28a27846def773
MShojaa <maziar.shojaa@gmail.com>
2025-02-18 03:06:18 +0330

    Update CMakeLists.txt for CTest

--- SmartWidget/prims/tests/prims_tests.cpp
+++ SmartWidget/prims/tests/prims_tests.cpp
@@ -1,18 +1,6 @@
 #include <catch2/catch_test_macros.hpp>
 
-TEST_CASE("test", "[test]") {
+TEST_CASE("test", "[prims_tests]")
+{
     SUCCEED("Yes!");
 }
-
-// #include <cassert>
-// #include <iostream>
-
-// void test() {
-//     assert(5 + 2 == 7);
-// }
-
-// int main()
-// {
-//     std::cout << "All tests passed\n";
-//     return 0;
-// }
